cdb4bbea057db75d068a1ca9a3daed400a90b79f
Debbie Neflas <debbieneflas@gmail.com>
2023-11-14 23:10:38 -0800

    navbar styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import logoIcon from "../assets/images/logo.png";
 const Footer = () => {
   return (
     <footer className="flex-row justify-center py-2">
-      <img src={logoIcon} alt="Bubs icon" width="50px" />
+      <a href="#">
+        <img src={logoIcon} alt="Bubs icon" width="50px" />
+      </a>
     </footer>
   );
 };
