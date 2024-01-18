1d856a57e9f96bda4ac4c841c5c985751fc2cfcf
Debbie Neflas <debbieneflas@gmail.com>
2024-01-17 21:48:18 -0800

    update footer with social logos

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import About from "./pages/About.jsx";
-import Team from "./pages/Team.jsx";
 import Home from "./pages/Home.jsx";
-import DownloadPage from "./pages/DownloadPage.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 
 // import "./index.css";
